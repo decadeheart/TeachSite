cf8bb84e1851e4c79537d203367c724c7eac18cf
Decade Chelse <Decade Chelse>
2017-11-10 15:33:58 +0800

    解决跨域问题

--- app.js
+++ app.js
@@ -197,3 +197,14 @@ app.delete('/deleteVedio/:id', function (req, res, next) {
     }
   });
 });
+//解决跨域问题
+app.all("*", function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+});
